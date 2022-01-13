5a4b747aba4409b14c78d2dc473bef88bc063a71
hassanbenhzaine <cbenhzaine@gmail.com>
2022-01-13 13:43:34 +0100

    fixed firstname and lastname in all html templates

--- src/main/java/com/youcode/gestionemployes/Main.java
+++ src/main/java/com/youcode/gestionemployes/Main.java
@@ -1,12 +1,23 @@
 package com.youcode.gestionemployes;
 
-import jakarta.persistence.Persistence;
-import jakarta.servlet.annotation.WebServlet;
+import com.youcode.gestionemployes.entity.Administrateur;
+import com.youcode.gestionemployes.metier.UtilisateurService;
 
-@WebServlet
 public class Main {
     public static void main(String[] args) {
-        Persistence.generateSchema("GestionEmployesPU", null);
+//        Persistence.generateSchema("GestionEmployesPU", null);
+
+        UtilisateurService utilisateurService = new UtilisateurService();
+        Administrateur admin = new Administrateur();
+        admin.setEmail("cbenhzaine@gmail.com");
+        admin.setPassword("");
+
+        utilisateurService.save(admin);
+
+
+//        employe.setMatricule("abcdedf");
+//        utilisateurService.update(employe);
+
 
     }
 }
